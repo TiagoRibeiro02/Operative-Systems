3aaa1631daf37aca1e6861d1db3343e48c10bfbf
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-03-21 10:50:28 +0000

    .

---
+++ FP03/servidor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main()
+{
+    char linha[200];
+    char s1[3] = "", s2[100] = "";
+    int a, b;
+    fgets(linha, 200, stdin);
+    sscanf(linha, "%s %d %d %s", s1, &a, &b, s2);
+    printf("%s %d %d %s\n", s1, a, b, s2);
+
+    int pid;
+    if (s1 == "NG")
+    {
+        pid = fork();
+        if (0 == pid)
+        {
+            int fd = open(s2, O_RDONLY);
+            printf("entrei aqui");
+        }
+        main();
+    }
+    else if (s1 == "NE")
+    {
+        return 0;
+    }
+    return 0;
+}
+
