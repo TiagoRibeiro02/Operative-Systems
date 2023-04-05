5773b3ce5da2d1ccebcdf7882d352b0922d15a9c
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-05 17:13:04 +0100

    Update parteA.c

--- TGrupo/parteA.c
+++ TGrupo/parteA.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 101
+
 int main(int argc, char *argv[])
 {
-  
+    if (argc != 2) {
+        write(STDOUT_FILENO, "Binary file requiered\n");
+        return 1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        write(STDOUT_FILENO, "Error opening file\n");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    int buffer_size = 0;
+
 }
