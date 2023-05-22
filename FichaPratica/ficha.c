611d23cb058f8bd986692976ec4a2affd68ec0a7
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-05-22 18:51:18 +0100

    .

--- FichaPratica/ficha.c
+++ FichaPratica/ficha.c
@@ -17,15 +17,54 @@ sem_t s;
 volatile int bilDisp = 45522;
 volatile int vendas[10] = {0};
 
+volatile int vez[NUM_THREADS];
+volatile int interessado[NUM_THREADS];
+
 int semente;
 int metodo;
 
 
 
-void *funcao3(void *args)
+void *funcao4(void *args)
 {
-    
+    int id = *(int *)args;
+    srand(semente + id);
+    int numVendidos_na_Thread = 0;
 
+    while (numVendidos_na_Thread < 1500) {
+        int b = 1 + rand() % 4;
+
+        if (numVendidos_na_Thread + b > 1500) {
+            b = 1500 - numVendidos_na_Thread;
+        }
+
+        numVendidos_na_Thread += b;
+
+        // Início da seção crítica
+        interessado[id] = 1;
+        vez[id] = 1;
+        for (int j = 0; j < NUM_THREADS; j++) {
+            if (j != id) {
+                vez[id] = j;
+                while (interessado[j] && vez[id] == j)
+                    ; // Espera ocupada
+            }
+        }
+
+        // Seção crítica
+        bilDisp -= b;
+        vendas[id] += b;
+
+        interessado[id] = 0;
+        // Fim da seção crítica
+    }
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+
+ void *funcao3(void *args){
     int id = *(int *)args;
 
     //printf("Thread %d\n", id);
@@ -51,7 +90,36 @@ void *funcao3(void *args)
 
 
     return (NULL);
-}   
+    
+ }   
+
+void peterson()
+{
+    pthread_t threads[NUM_THREADS];
+    int ids[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        ids[i] = i;
+        interessado[i] = 0;
+        vez[i] = 0;
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, funcao4, &ids[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    int total = 0;
+    for (int i = 0; i < 10; i++) {
+        total += vendas[i];
+    }
+    int capacidade = bilDisp + total;
+
+    fprintf(stderr, "Capacidade calculada %d (45522?)\n", capacidade);
+}
 
 
 void semaforo(){
@@ -165,7 +233,7 @@ int main (int argc ,char * argv[])
     int i, ids[NUM_THREADS];
     if(1== metodo) mutex();
     else if (2 == metodo) semaforo();
-   // else if(3 == metodo )
+    else if(3 == metodo ) peterson();
    else{
     for (i = 0; i < NUM_THREADS; i++) ids[i]=i;
     
